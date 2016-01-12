e37b86ac32eebe336a4b0b9005fcc154eb02f9a6
Mathieu Reguer <mathieu.reguer@gmail.com>
2016-01-12 17:21:05 +0100

    More typos

--- ExtentionSettingsManager.py
+++ ExtentionSettingsManager.py
@@ -5,7 +5,7 @@
 import plistlib
 
 from mojo.extensions import getExtensionDefault, setExtensionDefault
-from robofab.interface.all.doalogue import PutFile, GetFile
+from robofab.interface.all.dialogue import PutFile, GetFile
 
 
 #----------
