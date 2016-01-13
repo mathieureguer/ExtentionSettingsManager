410df3360e433f0faa7fc006723fd1d2b44a2f92
Mathieu Reguer <mathieu.reguer@gmail.com>
2016-01-13 02:01:29 +0100

    Tracking registered keys

--- ExtentionSettingsManager.py
+++ ExtentionSettingsManager.py
@@ -14,12 +14,14 @@ from robofab.interface.all.dialogs import PutFile, GetFile
 
 class ExtentionSettingsManager(object):
     """
-    Deals with an extention default.
+    Deals with an extention defaults.
+    Keep track of the registered keys
     Can import and export dict presets as xml
     """
+
     def __init__(self, defaultKeyBase):
         self.defaultKeyBase = defaultKeyBase
-
+        self.keys = set()
 
     # defaults
 
@@ -36,7 +38,22 @@ class ExtentionSettingsManager(object):
                     if e == "__fallback__":
                         setExtensionDefault(k, v)
 
-        registerExtensionsDefaults(defaults) 
+        registerExtensionsDefaults(defaults)
+        self.keys.update(dict.keys())
+ 
+
+    def set_defaults(self, dict):
+        defaults = self.prep_dict_with_prefix(dict, self.defaultKeyBase)
+        for k, v in defaults:
+            setExtensionDefault(k, v)
+        self.keys.update(dict.keys())
+
+
+    def get_defaults(self, fallback=None):
+        defaults = {}
+        for k in self.keys():
+            defaults[k] = self.get_default(k, fallback=fallback)
+        return defaults
 
 
     def get_default(self, key, fallback=None):
@@ -69,7 +86,7 @@ class ExtentionSettingsManager(object):
         f.write(self.dict_to_plist(dict))
         f.close()
 
-    def write_preset_dialogue(self, dict, message=None, fileName=None):
+    def write_preset_dialogue(self, dict, message="Save Preset File", fileName=None):
         path = PutFile(message=message, fileName=fileName)
         self.write_preset(dict, path)
 
@@ -78,6 +95,6 @@ class ExtentionSettingsManager(object):
         plist = f.read()
         return self.plist_to_dict(plist)
 
-    def read_preset_dialogue(self, message=None, title=None, fileTypes=None):
+    def read_preset_dialogue(self, message="Select Preset File", title=None, fileTypes=None):
         path = GetFile(message=message, title=title, fileTypes=fileTypes)
         return self.read_preset(path)
