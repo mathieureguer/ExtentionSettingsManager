448a4d1ffd4b21310e41618e39346207a8d80a45
Mathieu Reguer <mathieu.reguer@gmail.com>
2020-01-22 12:35:55 +0100

    Add rough tests

---
+++ test.py
@@ -0,0 +1,63 @@
+import unittest
+
+from ExtentionSettingsManager.ExtentionSettingsManager import ExtentionSettingsManager
+
+
+KEY_PREFIX = "com.mathieureguer.test."
+TEST_DATA = {"hello": "world",
+             "foo": "bar",
+             }
+
+TEST_DATA_PREFIXED = {KEY_PREFIX + "hello": "world",
+                      KEY_PREFIX + "foo": "bar",
+                      }
+
+
+class TestExtentionSettingManager(unittest.TestCase):
+
+    def test_dict_input(self):
+        """
+        Test that it can sum a list of integers
+        """
+        manager = ExtentionSettingsManager(KEY_PREFIX)
+        for k, v in TEST_DATA.items():
+            manager[k] = v
+        self.assertEqual(manager.data, TEST_DATA)
+
+    def test_dict_update(self):
+        """
+        Test that it can sum a list of integers
+        """
+        manager = ExtentionSettingsManager(KEY_PREFIX)
+        manager.update(TEST_DATA)
+        self.assertEqual(manager.data, TEST_DATA)
+
+    def test_dict_get(self):
+        """
+        Test that it can sum a list of integers
+        """
+        manager = ExtentionSettingsManager(KEY_PREFIX)
+        manager.update(TEST_DATA)
+        for k in manager.keys():
+            self.assertEqual(manager[k], TEST_DATA[k])
+
+    def test_dict_prefix(self):
+        """
+        Test that it can sum a list of integers
+        """
+        manager = ExtentionSettingsManager(KEY_PREFIX)
+        manager.update(TEST_DATA)
+
+        self.assertEqual(manager.data_with_prefixed_keys, TEST_DATA_PREFIXED)
+
+    def test_dict_cleaner(self):
+        """
+        Test that it can sum a list of integers
+        """
+        manager = ExtentionSettingsManager(KEY_PREFIX)
+        manager.data_from_prefix(TEST_DATA_PREFIXED)
+        self.assertEqual(manager.data, TEST_DATA)
+
+
+if __name__ == '__main__':
+    unittest.main()
